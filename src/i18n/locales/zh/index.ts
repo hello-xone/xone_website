ab6297c61395ff8f48155ee9758604a59f09fca2
wesson <stephentwesson@gmail.com>
2025-08-25 13:13:09 +0800

    feat: Verify channel style internationalization

--- src/i18n/locales/zh/index.ts
+++ src/i18n/locales/zh/index.ts
@@ -1,11 +1,13 @@
-import common from "./index.json";
-import home from "./home.json";
-import developer from "./developer.json";
+import channel from "./channel.json";
 import commercial from "./commercial.json";
+import developer from "./developer.json";
+import home from "./home.json";
+import common from "./index.json";
 
 export default {
   common,
   home,
   developer,
   commercial,
+  channel,
 };
