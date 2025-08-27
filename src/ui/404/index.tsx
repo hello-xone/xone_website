7793a6a3918d12130f96bba55013d034d2d03b5a
wesson <stephentwesson@gmail.com>
2025-08-27 10:50:38 +0800

    feat: 404 page

--- src/ui/404/index.tsx
+++ src/ui/404/index.tsx
@@ -1,53 +1,46 @@
-import { BaseContainer } from "@/components/layout/BaseContainer";
-import styles from "./index.module.less";
-import picture from "@/assets/imgs/404/404.png";
-import { useNavigate } from "react-router";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router";
+
+import { BaseContainer } from "@/components/layout/BaseContainer";
 import { EXTERNAL_LINKS } from "@/constants/external";
+
+import styles from "./index.module.less";
 
 export const NotFoundPage = () => {
   const navigate = useNavigate();
-  const { t,i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   return (
     <BaseContainer className={styles.wrapper}>
-      <div className={styles.left}>
-        <h1 className={styles.title}>{t("common:notFoundTitle")}</h1>
-        <p className={styles.description}>
-          {t("common:notFoundDescription")}{" "}
-          <span
-            className={styles.btn}
-            onClick={() => {
-              window.location.href = window.location.href;
-            }}
-          >
-            {t("common:refreshing")}
-          </span>{" "}
-          {t("common:thePageOr")}{" "}
-          <span
-            className={styles.btn}
-            onClick={() => navigate("/", { replace: true })}
-          >
-            {t("common:returnHemo")}
-          </span>
-          .
-        </p>
+      <div className={styles.pic}></div>
+      <h1 className={styles.title}>{t("common:notFoundTitle")}</h1>
+      <p className={styles.description}>
+        {t("common:notFoundDescription")}{" "}
+        <span
+          className={styles.btn}
+          onClick={() => {
+            window.location.href = window.location.href;
+          }}
+        >
+          {t("common:refreshing")}
+        </span>{" "}
+        {t("common:thePageOr")}{" "}
+        <span
+          className={styles.btn}
+          onClick={() => navigate("/", { replace: true })}
+        >
+          {t("common:returnHemo")}
+        </span>
+        .
+      </p>
 
-        <p className={styles.description}>
-          <a
-            className={styles.btn}
-            href={EXTERNAL_LINKS.dashboard + i18n.language + "/community"}
-            target="_blank"
-            rel="noopener noreferrer"
-            
-          >
-          {t("common:contactUsForFurtherAssistance")}
-          </a>{" "}
-          {t("common:help")}
-        </p>
-      </div>
-      <div className={styles.pic}>
-        <img className="w-full" src={picture} alt="" />
-      </div>
+      <a
+        className={styles.goToContact}
+        href={EXTERNAL_LINKS.dashboard + i18n.language + "/community"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {t("common:contactUsForFurtherAssistance")}
+      </a>
     </BaseContainer>
   );
 };
